ead4160a58c1e4ebce9b9758307550fc72d81f5b
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-04-19 09:52:16 -0300

    ajuste posição dos itens classe Liquidez

--- model/liquidez.js
+++ model/liquidez.js
@@ -1,7 +1,7 @@
 export default class Liquidez{
   constructor (liquidez_balanco, dre){
       console.log(liquidez_balanco)
-      this.liquidez_indices = liquidez_balanco.liquidez_indices
+      this.liquidez_indices = liquidez_balanco[0].liquidez_indices
       this.liquidez_corrente = this.liquidez_indices.indice_liquidez_corrente;
       this.liquidez_seca = this.liquidez_indices.indice_liquidez_seca;
       this.liquidez_imediata = this.liquidez_indices.indice_liquidez_imediata;
@@ -10,14 +10,7 @@ export default class Liquidez{
       this.patrimonio_social = this.liquidez_indices.imobilizacao_patrimonio_social;
       this.endividamento_geral = this.liquidez_indices.indice_endividamento_geral;
       this.solvencia_geral = this.liquidez_indices.indice_solvencia_geral;
-      this.lucratividade = dre.liquidez_indices.lucratividade;
+      this.lucratividade = dre[0].liquidez_indices.lucratividade;
   }
-
-  // info() {
-  //     console.log(this.nome);
-  //     console.log(this.liquidez_corrente);
-  //     console.log(this.liquidez_corrente.indice);
-  //     console.log(this.liquidez_corrente.resultado);
-  //     console.log(this.liquidez_corrente.situacao);
-  // }
 };
+
